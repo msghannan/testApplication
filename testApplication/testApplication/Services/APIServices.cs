a009b3a5f015891e78489c705c4deeed8a78ae00
M. Ghannan <msghannan@hotmail.com>
2020-05-26 15:52:16 +0200

    lite ändirngar

--- testApplication/testApplication/Services/APIServices.cs
+++ testApplication/testApplication/Services/APIServices.cs
@@ -4,14 +4,17 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using testApplication.Model;
 using testApplication.ViewModels;
 
 namespace testApplication.Models
 {
     public class APIServices
     {
+        private static string PostUrl = "https://localhost:44363/api/Tests";
         private static string BaseUrl = "https://localhost:44363/api";
         private static string Accounts = "/Accounts";
         public async Task<Person> LoginAsync(string username, string password)
@@ -43,5 +46,16 @@ namespace testApplication.Models
             return p;
         }
 
+        public async Task AddTestAsync(Test t)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var test = JsonConvert.SerializeObject(t);
+                HttpContent httpContent = new StringContent(test);
+                httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                var result = await client.PostAsync(PostUrl, httpContent);
+            }
+
+        }
     }
 }
